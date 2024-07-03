f47690a0dc146657ae6d2af5979071e483913833
BrunoDefenser26 <brunoprobst356@gmail.com>
2024-07-02 22:27:12 -0300

    Aula Sobre criação e movimento de entidades

--- StarDragon/code/EntityFactory.py
+++ StarDragon/code/EntityFactory.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Background import  Background
-
-
 class EntityFactory:
 
-    @staticmethod
-    def get_entity(self, entity_name: str, position: tuple):
-        match entity_name:
-            case    'Level1Bg':
-                Background(f'Level1Bg0', position)
+    def __int__(self):
         pass
 
+    def get_entity(self, entity_type):
+        pass
--- StarDragon/code/Game.py
+++ StarDragon/code/Game.py
@@ -14,22 +14,18 @@ from code.Menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.Window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
 
         while True:
-            menu = Menu(self.Window)
+            menu = Menu(self.window)
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.Window, 'Level1', menu_return)
-                level_return = level.run()
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = Level.run()
+
             else:
                 pygame.quit()
                 sys.exit()
-
-
-
-
-
